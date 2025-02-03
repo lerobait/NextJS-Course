26142658e3e8c21250853bc8307bf99a9e981b2f
Nikulin Artem <48065996+lerobait@users.noreply.github.com>
2025-02-03 15:42:15 +0200

    Server Actions in Great Detail
    -Deleting a Record with a Server Action
    -Calling a Server Action from a Client Component
    -Server Actions in a Separate File

--- snippets/src/components/snippet-edit-form.tsx
+++ snippets/src/components/snippet-edit-form.tsx
@@ -1,15 +1,38 @@
 "use client";
 
 import type { Snippet } from "@prisma/client";
-import MonacoEditorComponent from "./MonacoEditor";
+import { useState } from "react";
+import * as actions from "@/actions";
+import { Editor } from "@monaco-editor/react";
+
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code);
+
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
+
   return (
     <div>
-      <MonacoEditorComponent value={snippet.code} />
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        defaultLanguage="javascript"
+        defaultValue={snippet.code}
+        options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
+      />
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
